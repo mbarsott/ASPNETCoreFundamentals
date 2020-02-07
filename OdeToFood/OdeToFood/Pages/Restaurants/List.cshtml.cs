5c3293ea82770c4b7759621d83c9940ca8b58865
Marcelo Barsotti <mbarsott@gmail.com>
2020-02-06 21:21:10 -0500

    2. Drilling into Data

--- OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -1,20 +1,29 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using OdeToFood.Core;
+using OdeToFood.Data;
+using System.Collections.Generic;
 
 namespace OdeToFood.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
         private readonly IConfiguration config;
+        private readonly IRestaurantData restaurantData;
 
-        public ListModel(IConfiguration config)
+        public string Message { get; set; }
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+
+        public ListModel(IConfiguration config, IRestaurantData restaurantData)
         {
             this.config = config;
+            this.restaurantData = restaurantData;
         }
-        public string Message { get; set; }
+
         public void OnGet()
         {
             Message = config["Message"];
+            Restaurants = restaurantData.GetAll();
         }
     }
 }
