9d1c90b3a205b587889456d70b6ada2d6ba745ad
Marcelo Barsotti <mbarsott@gmail.com>
2020-02-09 13:29:36 -0500

    3. Working with Models and Model Binding

--- OdeToFood/OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood/OdeToFood.Data/IRestaurantData.cs
@@ -7,6 +7,7 @@ namespace OdeToFood.Data
     public interface IRestaurantData
     {
         IEnumerable<Restaurant>GetReataurantsByName(string name);
+        Restaurant GetById(int id);
     }
 
     public class InMemoryRestaurantData : IRestaurantData
@@ -22,6 +23,12 @@ namespace OdeToFood.Data
                 new Restaurant {Id=3, Name="La Costa", Location="California", Cuisine=Restaurant.CuisineType.Mexican}
             };
         }
+
+        public Restaurant GetById(int id )
+        {
+            return restaurants.SingleOrDefault(r => r.Id == id);
+        }
+
         public IEnumerable<Restaurant> GetReataurantsByName(string name = null)
         {
             return from r in restaurants
